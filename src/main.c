7cb5bd22acc28fc291e7499291776642cbd7d0f5
milanamilka <milamilanan@gmail.com>
2021-01-28 18:17:47 +0300

    add src

---
+++ src/main.c
@@ -0,0 +1,12 @@
+//программа заменяет части никнейма на другие символы
+#include <stdio.h>
+int main() {
+  char arr[] = "MILKA"; 
+  arr[1] = '1'; 
+  arr[4] = '@';
+  printf("%s\n", arr);
+  return 0;
+  }
+
+  
+
